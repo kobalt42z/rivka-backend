4456882140ce912e33b3708f33193347ac0a080e
Shmouel kessous <chmouel2690@gmail.com>
2023-03-29 00:30:37 +0300

    adapt create and product schema

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,10 +1,42 @@
-import { IsAlpha, IsAlphanumeric, IsArray, IsNotEmpty, IsNumber, IsOptional, IsString, IsUrl, Max, MaxLength, MIN, MinLength, } from "class-validator"
+import { Type } from "class-transformer";
+import { IsAlpha, IsAlphanumeric, IsArray, IsNotEmpty, IsNumber, IsOptional, IsString, IsUrl, max, Max, MaxLength, MIN, MinLength, ValidateNested, } from "class-validator"
 
+
+export class LangueDto {
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(2)
+    language: string
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(100)
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(100)
+    description: string;
+}
+export class TranslationDto {
+    @IsOptional()
+    @ValidateNested()
+    @Type(() => LangueDto)
+    fr: LangueDto
+
+    @IsOptional()
+    @ValidateNested()
+    @Type(() => LangueDto)
+    en: LangueDto
+}
 export class CreateProductDto {
     @IsNotEmpty()
     @IsString()
     @MaxLength(30)
     name: string
+
+
     @IsNotEmpty()
     @IsString()
     @MaxLength(20)
@@ -15,28 +47,57 @@ export class CreateProductDto {
     @MaxLength(100)
     description: string
 
-   
+    @IsNotEmpty()
+    @IsNumber()
+    @Max(100000)
+    base_price: number
 
     @IsNotEmpty()
     @IsNumber()
     @Max(100000)
-    price_ils: number
+    selling_price: number
 
     @IsNotEmpty()
     @IsNumber()
     @Max(100)
     reduction_p: number
 
-    @IsUrl() // !expermiental
-    @IsOptional()
-    @MaxLength(200)
-    imgUrl?: string
-    
     @IsNotEmpty()
     @IsNumber()
     @Max(100000)
     supply: number
 
+    @IsUrl() // !expermiental
+    @IsOptional()
+    @MaxLength(200)
+    imgUrl?: string
+
+
     @IsArray()
-    categoryIds:string[]
+    categoryIds: string[]
+
+    @IsOptional()
+    @IsArray()
+    colors: string[]
+
+    @IsOptional()
+    @IsArray()
+    sizes: string[]
+
+    @IsOptional()
+    @IsArray()
+    curves: string[]
+
+    @IsOptional()
+    @IsArray()
+    thickness: string[]
+
+    @ValidateNested()
+    @IsOptional()
+    @Type(() => TranslationDto)
+    translations: TranslationDto
+
+    @IsNotEmpty()
+    active: boolean
 }
+
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,6 +6,8 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { JoinCategoryDto } from './dto/join-category.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { TranslationDto } from './dto/create-product.dto'
+import { create } from 'domain';
 
 
 @Injectable()
@@ -17,13 +19,30 @@ export class ProductsService {
 
   async createAndConnect(createProductDto: CreateProductDto) {
     try {
-      const categories = createProductDto.categoryIds.map((id) => ({ id }))
+      const categories = createProductDto.categoryIds.map((id) => ({ id }));
+      const translatArr = [
+        {
+          languages: createProductDto.translations.fr.language,
+          description: createProductDto.translations.fr.description,
+          name: createProductDto.translations.fr.name
+        },
+        {
+          languages: createProductDto.translations.en.language,
+          description: createProductDto.translations.en.description,
+          name: createProductDto.translations.en.name
+        }
+      ];
+
       const product = await this.prisma.product.create({
         data: {
           ...createProductDto,
-          categorys: { connect: categories }
+          categorys: {
+            connect: categories
+          },
+          translations: {
+            create:translatArr
+          }
         }
-
       });
 
       return { msg: "product created succesfully!", product }
@@ -73,15 +92,19 @@ export class ProductsService {
     }
   }
 
-  async findAll(_skip:number) {
+  async findAll(_skip: number) {
     try {
       const products = await this.prisma.product.findMany({
-        take:10,
-        skip:_skip*10 || 0,
+        take: 10,
+        skip: _skip * 10 || 0,
+        include:{
+          translations:true,
+          categorys:{select:{name:true}}
+        }
 
       });
       const count = await this.prisma.product.count();
-      return {products,count};
+      return { products, count };
     } catch (error) {
       throw error
     }
@@ -100,15 +123,35 @@ export class ProductsService {
       const product = await this.prisma.product.findUniqueOrThrow({ where: { id: _id }, include: { categorys: true } });
       return product;
     } catch (error) {
-      throw error ;
+      throw error;
     }
   }
 
   async update(_id: string, updateProductDto: UpdateProductDto) {
     try {
+      let categories: { id: string }[] | never;
+      if (updateProductDto.categoryIds) {
+        categories = updateProductDto.categoryIds.map((id) => ({ id }));
+      }
+      const transArr = [
+        updateProductDto.translations.fr,
+        updateProductDto.translations.en,
+      ]
       const updatedProduct = await this.prisma.product.update({
         where: { id: _id },
-        data: updateProductDto
+        data: {
+          ...updateProductDto,
+          translations: {
+            updateMany: {
+              where: {},
+              data: { ...updateProductDto.translations }
+            }
+          },
+
+          categorys: {
+            connect: categories
+          }
+        }
       });
       return { action_status: "updated successfully !", updatedProduct };
     } catch (error) {
