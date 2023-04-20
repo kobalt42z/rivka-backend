d964c253c9fced10196e76478048b8f1afc4fbce
Shmouel kessous <chmouel2690@gmail.com>
2023-04-20 15:42:00 +0300

    try

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors:true});
   // app.useGlobalPipes(new ValidationPipe({
   //   whitelist:true, 
   //   enableDebugMessages:true,
@@ -22,13 +22,11 @@ async function bootstrap() {
   app.useGlobalFilters( new PrismaClientExceptionFilter(httpAdapter,{
     P2023:HttpStatus.BAD_REQUEST
   }))
-  app.enableCors({
-    origin: '*',
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
-    
-  });
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //   preflightContinue: true,
+  // });
   await app.listen(3333);
 }
 bootstrap();
