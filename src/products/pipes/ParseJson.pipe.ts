f5a2278fa7b8e1cf6dccf840e04139be8242e0ae
Shmouel kessous <chmouel2690@gmail.com>
2023-06-07 21:42:06 +0300

    test

--- src/products/pipes/ParseJson.pipe.ts
+++ src/products/pipes/ParseJson.pipe.ts
@@ -4,11 +4,9 @@ import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from
 export class parseJsonPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     if(!value.json_body) throw new BadRequestException("json_body is missing");
-    console.log(value.json_body);
-    
+
     const Output = JSON.parse(value.json_body)
-    // console.log(typeof Output);
-    
+
     return Output
   }
 }
